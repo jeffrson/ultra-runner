5f9aa31c197f2de6fe121761125043e0fb4e6f96
Ulf Jaenicke-Roessler <ujr@insynergie.de>
2026-05-26 19:56:01 +0200

    feat: add compiled output

---
+++ lib/terminal.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="node" />
+export declare function showCursor(stream?: NodeJS.WriteStream): void;
+export declare function hideCursor(stream?: NodeJS.WriteStream): void;
+export declare class Terminal {
+    stream: NodeJS.WriteStream & {
+        fd: 1;
+    };
+    options: {
+        clearScreen: boolean;
+    };
+    lines: string[];
+    output: string;
+    resized: boolean;
+    constructor(stream?: NodeJS.WriteStream & {
+        fd: 1;
+    }, options?: {
+        clearScreen: boolean;
+    });
+    clearScreen(): void;
+    diff(from: string, to: string): {
+        left: number;
+        str: string;
+    } | undefined;
+    reset(): void;
+    update(text: string | string[]): string | undefined;
+}
